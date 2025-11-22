0e91ecd549a1f8284e3c195c8c4a3681eed509b8
apradoc-es <apradoc-es@users.noreply.github.com>
2025-11-22 17:33:13 -0400

    feat: ajustes generales de estilo y color, formato para uuid

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,20 @@
 export default {
   content: [
     "./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte,md,mdx}",
-    "./public/**/*.html" // Si usas HTML en public
+    "./public/**/*.html"
   ],
   theme: {
     extend: {
       colors: {
         gray95: '#f2f2f2',
         platinum: '#e5e5e5',
+        // Nuevos colores corporativos
+        'dag-black': '#000000',
+        'dag-burgundy': '#800020', // o el código exacto que uses
       },
+    },
+    fontFamily: {
+      advent: ['"Advent Pro"', 'sans-serif'],
     },
   },
   plugins: [],
