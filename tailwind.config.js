91c5742d26a47542c9a2300f8d93b33c2061a80b
apradoc-es <apradoc-es@users.noreply.github.com>
2025-11-23 03:42:13 -0400

    feat: Nueva sección de contacto, estilos páginas, sitemap al pie

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,21 @@ export default {
   ],
   theme: {
     extend: {
+      fontSize: {
+        'h1': ['3.2rem', { lineHeight: '1.2', fontWeight: '700' }],
+        'h2': ['2.4rem',   { lineHeight: '1.25', fontWeight: '700' }],
+        'h3': ['1.6rem', { lineHeight: '1.3', fontWeight: '700' }],
+      },
       colors: {
         gray95: '#f2f2f2',
         platinum: '#e5e5e5',
-        // Nuevos colores corporativos
         'dag-black': '#000000',
-        'dag-burgundy': '#800020', // o el código exacto que uses
+        'dag-burgundy': '#800020',
       },
     },
     fontFamily: {
       advent: ['"Advent Pro"', 'sans-serif'],
+      // Montserrat seguirá siendo la fuente por defecto para el cuerpo
     },
   },
   plugins: [],
